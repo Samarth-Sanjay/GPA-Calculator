8f642822a80c208b97ac2c5f69eabdd7588e66e9
samarth sanjay <samarth.sanjay95@outlook.com>
2018-11-25 21:30:27 -0500

    Added form 1 with initial page

---
+++ GPA-Calculator/GPA-Calculator/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace GPA_Calculator
+{
+    partial class SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSignIn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxUnum2 = new System.Windows.Forms.TextBox();
+            this.textBoxUnum1 = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelUNumber = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSignIn
+            // 
+            this.buttonSignIn.Location = new System.Drawing.Point(376, 332);
+            this.buttonSignIn.Name = "buttonSignIn";
+            this.buttonSignIn.Size = new System.Drawing.Size(139, 51);
+            this.buttonSignIn.TabIndex = 19;
+            this.buttonSignIn.Text = "Sign In";
+            this.buttonSignIn.UseVisualStyleBackColor = true;
+            this.buttonSignIn.Click += new System.EventHandler(this.buttonSignIn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(482, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 42);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "-";
+            // 
+            // textBoxUnum2
+            // 
+            this.textBoxUnum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUnum2.Location = new System.Drawing.Point(518, 249);
+            this.textBoxUnum2.Name = "textBoxUnum2";
+            this.textBoxUnum2.Size = new System.Drawing.Size(100, 49);
+            this.textBoxUnum2.TabIndex = 17;
+            // 
+            // textBoxUnum1
+            // 
+            this.textBoxUnum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUnum1.Location = new System.Drawing.Point(376, 249);
+            this.textBoxUnum1.Name = "textBoxUnum1";
+            this.textBoxUnum1.Size = new System.Drawing.Size(100, 49);
+            this.textBoxUnum1.TabIndex = 16;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLastName.Location = new System.Drawing.Point(338, 163);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(280, 49);
+            this.textBoxLastName.TabIndex = 15;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFirstName.Location = new System.Drawing.Point(338, 67);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(280, 49);
+            this.textBoxFirstName.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(331, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 42);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "U";
+            // 
+            // labelUNumber
+            // 
+            this.labelUNumber.AutoSize = true;
+            this.labelUNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUNumber.Location = new System.Drawing.Point(183, 256);
+            this.labelUNumber.Name = "labelUNumber";
+            this.labelUNumber.Size = new System.Drawing.Size(142, 31);
+            this.labelUNumber.TabIndex = 12;
+            this.labelUNumber.Text = "U-number:";
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLastName.Location = new System.Drawing.Point(183, 163);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(153, 31);
+            this.labelLastName.TabIndex = 11;
+            this.labelLastName.Text = "Last Name:";
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFirstName.Location = new System.Drawing.Point(183, 70);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(155, 31);
+            this.labelFirstName.TabIndex = 10;
+            this.labelFirstName.Text = "First Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSignIn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxUnum2);
+            this.Controls.Add(this.textBoxUnum1);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelUNumber);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSignIn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxUnum2;
+        private System.Windows.Forms.TextBox textBoxUnum1;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelUNumber;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.Label labelFirstName;
+    }
+}
+
---
+++ GPA-Calculator/GPA-Calculator/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPA_Calculator
+{
+    public partial class SignIn : Form
+    {
+        public SignIn()
+        {
+            InitializeComponent();
+            textBoxFirstName.Focus();
+        }
+
+        private void displayErrorBox(string errorMessage)
+        {
+            MessageBox.Show(errorMessage, "Entry Error");
+        }
+        public bool isValid()
+        {
+            var firstName = textBoxFirstName.Text;
+            var lastName = textBoxLastName.Text;
+            var uNumberTextBox1 = textBoxUnum1.Text;
+            var uNumberTextBox2 = textBoxUnum2.Text;
+
+            //decimal checkDecimal = 0m;
+            if (String.IsNullOrWhiteSpace(firstName))
+            {
+                displayErrorBox("First Name is a required field.");
+                return false;
+            }
+            else if (!Char.IsLetter(firstName.First()))
+            {
+                displayErrorBox("First Name must contain only letters");
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(lastName))
+            {
+                displayErrorBox("Last Name is a required field.");
+                return false;
+            }
+            else if (!Char.IsLetter(lastName.First()))
+            {
+                displayErrorBox("Last Name must contain only letters");
+                return false;
+            }
+            else if (String.IsNullOrWhiteSpace(uNumberTextBox1))
+            {
+                displayErrorBox("First half of U-number is a required field.");
+                return false;
+            }
+            else if (uNumberTextBox1.Length != 4)
+            {
+                displayErrorBox("First half of U-Number must contain 4 digits");
+                return false;
+            }
+            else if (String.IsNullOrWhiteSpace(uNumberTextBox2))
+            {
+                displayErrorBox("Second half of U-number is a required field.");
+                return false;
+            }
+
+
+
+            else if (uNumberTextBox2.Length != 4)
+            {
+                displayErrorBox("Second half of U-Number must contain 4 digits");
+                return false;
+            }
+            return true;
+
+        }
+        private void buttonSignIn_Click(object sender, EventArgs e)
+        {
+            var firstName = textBoxFirstName.Text.Trim();
+            var lastName = textBoxLastName.Text.Trim();
+            var unumber = textBoxUnum1.Text.Trim();
+            var unum2 = textBoxUnum2.Text.Trim();
+            if (isValid())
+            {
+                Form2 dataEntry = new Form2();
+                this.Hide();
+                dataEntry.Text = $"Hello {firstName} {lastName} (U{unumber}-{unum2})";
+                dataEntry.ShowDialog();
+
+            }
+        
+        }
+    }
+}
